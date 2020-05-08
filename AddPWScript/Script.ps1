802e25cfa024dfe9f7af6888909a5c84f996f946
Peter Wawa <meelisn@outlook.com>
2020-05-08 17:13:46 +0300

    Refactor template script
    - rearrange comment-based help
    - rearrange metadata block
    - add sample parameters and pipeline processing

--- AddPWScript/Script.ps1
+++ AddPWScript/Script.ps1
@@ -3,16 +3,21 @@
 <#PSScriptInfo
     .VERSION <%=$PLASTER_PARAM_Version%>
     .GUID <%=$PLASTER_Guid1%>
+
     .AUTHOR <%=$PLASTER_PARAM_FullName%>
     .COMPANYNAME <%=$PLASTER_PARAM_ScriptCompanyName%>
     .COPYRIGHT (c) <%=$PLASTER_PARAM_ScriptCompanyName%> <%=$PLASTER_Year%>.  All rights reserved.
+
     .TAGS
+
     .LICENSEURI https://opensource.org/licenses/MIT
     .PROJECTURI
     .ICONURI
+
     .EXTERNALMODULEDEPENDENCIES
     .REQUIREDSCRIPTS
     .EXTERNALSCRIPTDEPENDENCIES
+
     .RELEASENOTES
 
     .PRIVATEDATA
@@ -20,9 +25,10 @@
 
 <#
     .SYNOPSIS
+        <%=$PLASTER_PARAM_ScriptDesc%>
 
     .DESCRIPTION
-        <%=$PLASTER_PARAM_ScriptDesc%>
+        Longer Description
 
     .EXAMPLE
         PS C:\> <%=$PLASTER_PARAM_ScriptName%>
@@ -41,7 +47,64 @@
         Powershell Online Help: http://microsoft.com/powershell
 #>
 
-[CmdLetBinding()]
-Param()
+[CmdletBinding(
+    DefaultParameterSetName='Parameter Set 1',
+    SupportsShouldProcess=$true,
+    PositionalBinding=$false,
+    HelpUri = 'http://www.microsoft.com/',
+    ConfirmImpact='Medium'
+)]
+[OutputType([String])]
 
-Write-Warning -Message "Not yet implemented"
+param (
+        [Parameter(
+            Mandatory=$true,
+            Position=0,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ValueFromRemainingArguments=$false,
+            ParameterSetName='Parameter Set 1'
+        )]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateCount(0,5)]
+        [ValidateSet("sun", "moon", "earth")]
+        [Alias("p1")]
+        # Param1 help description
+    $Param1,
+
+        [Parameter(ParameterSetName='Parameter Set 1')]
+        [AllowNull()]
+        [AllowEmptyCollection()]
+        [AllowEmptyString()]
+        [ValidateScript({
+            $true
+        })]
+        [ValidateRange(0,5)]
+        [int]
+        # Param2 help description
+    $Param2,
+
+        [Parameter(ParameterSetName='Another Parameter Set')]
+        [ValidatePattern("[a-z]*")]
+        [ValidateLength(0,15)]
+        [string]
+        # Param3 help description
+    $Param3
+)
+
+begin {
+    Write-Verbose -Message 'Starting function'
+}
+
+process {
+    if ($PSCmdlet.ShouldProcess("Target", "Operation")) {
+        Write-Verbose -Message 'Operating with target'
+    }
+
+    Write-Warning -Message 'Not yet implemented'
+}
+
+end {
+    Write-Verbose 'Ending function'
+}
--- PWScript/src/Script.ps1
+++ PWScript/src/Script.ps1
@@ -1,47 +1,110 @@
-﻿
+﻿#Requires -Version <%=$PLASTER_PARAM_ScriptPsVersion%>
+
 <#PSScriptInfo
+    .VERSION <%=$PLASTER_PARAM_Version%>
+    .GUID <%=$PLASTER_Guid1%>
 
-.VERSION <%=$PLASTER_PARAM_Version%>
+    .AUTHOR <%=$PLASTER_PARAM_FullName%>
+    .COMPANYNAME <%=$PLASTER_PARAM_ScriptCompanyName%>
+    .COPYRIGHT (c) <%=$PLASTER_PARAM_ScriptCompanyName%> <%=$PLASTER_Year%>.  All rights reserved.
 
-.GUID <%=$PLASTER_Guid1%>
+    .TAGS
 
-.AUTHOR <%=$PLASTER_PARAM_FullName%>
+    .LICENSEURI https://opensource.org/licenses/MIT
+    .PROJECTURI
+    .ICONURI
 
-.COMPANYNAME <%=$PLASTER_PARAM_ScriptCompanyName%>
+    .EXTERNALMODULEDEPENDENCIES
+    .REQUIREDSCRIPTS
+    .EXTERNALSCRIPTDEPENDENCIES
 
-.COPYRIGHT (c) <%=$PLASTER_PARAM_ScriptCompanyName%> <%=$PLASTER_Year%>.  All rights reserved.
+    .RELEASENOTES
 
-.TAGS
-
-.LICENSEURI
-
-.PROJECTURI
-
-.ICONURI
-
-.EXTERNALMODULEDEPENDENCIES
-
-.REQUIREDSCRIPTS
-
-.EXTERNALSCRIPTDEPENDENCIES
-
-.RELEASENOTES
-
-
-.PRIVATEDATA
-
+    .PRIVATEDATA
 #>
 
 <#
+    .SYNOPSIS
+        <%=$PLASTER_PARAM_ScriptDesc%>
 
-.DESCRIPTION
- <%=$PLASTER_PARAM_ScriptDesc%>
+    .DESCRIPTION
+        Longer Description
 
+    .EXAMPLE
+        PS C:\> <%=$PLASTER_PARAM_ScriptName%>
+        Explanation of what the example does
+
+    .INPUTS
+        Inputs (if any)
+
+    .OUTPUTS
+        Output (if any)
+
+    .NOTES
+        General notes
+
+    .LINK
+        Powershell Online Help: http://microsoft.com/powershell
 #>
 
-#Requires -Version <%=$PLASTER_PARAM_ScriptPsVersion%>
+[CmdletBinding(
+    DefaultParameterSetName='Parameter Set 1',
+    SupportsShouldProcess=$true,
+    PositionalBinding=$false,
+    HelpUri = 'http://www.microsoft.com/',
+    ConfirmImpact='Medium'
+)]
+[OutputType([String])]
 
-[CmdLetBinding()]
-Param()
+param (
+        [Parameter(
+            Mandatory=$true,
+            Position=0,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ValueFromRemainingArguments=$false,
+            ParameterSetName='Parameter Set 1'
+        )]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateCount(0,5)]
+        [ValidateSet("sun", "moon", "earth")]
+        [Alias("p1")]
+        # Param1 help description
+    $Param1,
 
-Write-Verbose -Message "Not yet implemented"
+        [Parameter(ParameterSetName='Parameter Set 1')]
+        [AllowNull()]
+        [AllowEmptyCollection()]
+        [AllowEmptyString()]
+        [ValidateScript({
+            $true
+        })]
+        [ValidateRange(0,5)]
+        [int]
+        # Param2 help description
+    $Param2,
+
+        [Parameter(ParameterSetName='Another Parameter Set')]
+        [ValidatePattern("[a-z]*")]
+        [ValidateLength(0,15)]
+        [string]
+        # Param3 help description
+    $Param3
+)
+
+begin {
+    Write-Verbose -Message 'Starting function'
+}
+
+process {
+    if ($PSCmdlet.ShouldProcess("Target", "Operation")) {
+        Write-Verbose -Message 'Operating with target'
+    }
+
+    Write-Warning -Message 'Not yet implemented'
+}
+
+end {
+    Write-Verbose 'Ending function'
+}
